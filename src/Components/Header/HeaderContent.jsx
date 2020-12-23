e875c9a248351af70f344011e65f622da6d257c9
Daniel <itzdanielmichael@gmail.com>
2020-12-22 21:25:30 -0500

    refactor fillcheck

--- src/Components/Header/HeaderContent.jsx
+++ src/Components/Header/HeaderContent.jsx
@@ -4,10 +4,7 @@ import Fill2 from "./Fill/Fill2";
 import HeaderText from "./Text/HeaderText";
 import Scroll from "./Scroll/Scroll";
 import MoveInLeft from "../shared/Animations/MoveInLeft";
-import About from "../Home/About";
-import Stack from "../Stack/Stack";
 import styled from "styled-components";
-import { onResize } from "../../utils/onResize";
 
 const CloudBig = styled.img`
   @keyframes float {
@@ -27,7 +24,6 @@ const CloudBig = styled.img`
       transform: translatey(0px);
     }
   }
-  /* margin-top: -200px; */
   z-index: -1;
   filter: blur(4px);
   z-index: 0;
@@ -37,43 +33,6 @@ const CloudBig = styled.img`
   box-shadow: none;
   animation: float 6s ease-in-out infinite;
   margin-top: -60px;
-`;
-
-const SmallCloudsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-const CloudSmall = styled.img`
-  @keyframes float {
-    0% {
-      transform: translatey(0px);
-    }
-    50% {
-      transform: translatey(-20px);
-    }
-    100% {
-      transform: translatey(0px);
-    }
-  }
-
-  width: 150px;
-  height: 150px;
-  filter: blur(3.05px);
-  margin-top: -400px;
-  z-index: 0;
-  opacity: 0.7;
-  cursor: pointer;
-  z-index: 2;
-  margin-left: 300px;
-  transition: transform 2s ease-out;
-
-  transform: translatey(0px);
-  animation: float 6s ease-in-out infinite;
-
-  &:hover {
-    transform: scale(2);
-    transition: transform 250s ease-in;
-  }
 `;
 
 const Div = styled.div`
@@ -103,21 +62,18 @@ export default function HeaderContent() {
     let width = document.body.clientWidth;
     if (width <= 768) {
       setDisabled(true);
-      onResize();
     }
     if (width >= 768) {
       setDisabled(false);
-      onResize();
     }
   }
-  window.addEventListener("resize", fillCheck);
 
   React.useEffect(() => {
-    onResize();
     fillCheck();
     window.addEventListener("resize", () => {
+      fillCheck();
       return () => {
-        window.removeEventListener("resize", onResize);
+        window.removeEventListener("resize", fillCheck);
       };
     });
   }, []);
@@ -126,15 +82,7 @@ export default function HeaderContent() {
     <>
       <MoveInLeft className="animation1">
         <HeaderText />
-        {/* <About /> */}
         <Scroll />
-        <SmallCloudsContainer>
-          {/* <CloudSmall className="cloud" src="https://i.imgur.com/UOQ3aCS.png" /> */}
-          {/* <CloudSmall className="cloud" src="https://i.imgur.com/UOQ3aCS.png" /> */}
-          {/* <CloudSmall className="cloud" src="https://i.imgur.com/UOQ3aCS.png" /> */}
-          {/* <CloudSmall className="cloud" src="https://i.imgur.com/UOQ3aCS.png" /> */}
-        </SmallCloudsContainer>
-
         <Fill1 fill1="https://anima-uploads.s3.amazonaws.com/projects/5fb14441119f80c2053ea467/releases/5fb14452ac34b30698d1c801/img/01maindemo-fill-1-3E925590-1D1F-4FAB-9036-050A24FF5082.png" />
         {navigator?.userAgent?.indexOf("Firefox") !== -1 ? (
           <></>
