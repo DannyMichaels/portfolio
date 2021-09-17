dc214dad62e854c8f4516ea7a97e115692825d36
Daniel Michael <itzdanielmichael@gmail.com>
2021-09-16 20:15:07 -0400

    Update Stack.jsx

--- src/Components/Stack/Stack.jsx
+++ src/Components/Stack/Stack.jsx
@@ -135,7 +135,7 @@ function Stack() {
         </Tooltip>
         <Tooltip title="JavaScript">
           <img
-            src="https://www.devexhub.com/wp-content/uploads/2019/12/javascript-icon-png-23.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
             alt="JavaScript"
           />
         </Tooltip>
