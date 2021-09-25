c59ced09283ce5e306fd0c47a458f7db1f0b3ca1
Daniel <itzdanielmichael@gmail.com>
2021-09-24 21:36:04 -0400

    update Sparkles.jsx

--- src/Components/shared/Animations/Sparkles.jsx
+++ src/Components/shared/Animations/Sparkles.jsx
@@ -38,7 +38,10 @@ const Sparkles = ({ color = DEFAULT_COLOR, children, ...delegated }) => {
   const [sparkles, setSparkles] = useState(() => {
     return range(3).map(() => generateSparkle(color));
   });
+
   const prefersReducedMotion = usePrefersReducedMotion();
+
+  // generate sparkles on random intervals.
   useRandomInterval(
     () => {
       const sparkle = generateSparkle(color);
@@ -59,6 +62,7 @@ const Sparkles = ({ color = DEFAULT_COLOR, children, ...delegated }) => {
     prefersReducedMotion ? null : 50,
     prefersReducedMotion ? null : 450
   );
+
   return (
     <Wrapper {...delegated}>
       {sparkles.map((sparkle) => (
@@ -73,6 +77,7 @@ const Sparkles = ({ color = DEFAULT_COLOR, children, ...delegated }) => {
     </Wrapper>
   );
 };
+
 const Sparkle = ({ size, color, style }) => {
   const path =
     'M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z';
