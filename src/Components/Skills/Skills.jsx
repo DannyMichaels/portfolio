3f98f1cd56489c73c8605c8adb2f962d299cb500
Daniel <itzdanielmichael@gmail.com>
2021-09-25 11:08:23 -0400

    update Skills.jsx, add Logos

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,8 +3,119 @@ import Tooltip from '@material-ui/core/Tooltip';
 import styled from 'styled-components';
 import { CloudStateContext } from '../../context/animationContext';
 import Sparkles from '../shared/Animations/Sparkles';
+
+// logos
 import MuiLogo from '../../assets/images/tech_skills/Mui.png';
 import StyledComponentsLogo from '../../assets/images/tech_skills/styled-components.png';
+import RubyLogo from '../../assets/images/tech_skills/ruby.png';
+import RubyOnRailsLogo from '../../assets/images/tech_skills/ruby-on-rails.png';
+import BootstrapLogo from '../../assets/images/tech_skills/bootstrap.png';
+import JavaScriptLogo from '../../assets/images/tech_skills/javascript.png';
+
+export default function Skills() {
+  const [cloudMode] = useContext(CloudStateContext);
+
+  return (
+    <Section className="language" cloudMode={cloudMode}>
+      <div className="card">
+        <h1>
+          <Sparkles>
+            <div className="text-letter">S</div>
+            <div className="text-letter">k</div>
+            <div className="text-letter">i</div>
+            <div className="text-letter">l</div>
+            <div className="text-letter">l</div>
+            <div className="text-letter">s</div>
+          </Sparkles>
+        </h1>
+        <br />
+        <br />
+
+        <Tooltip arrow placement="top" title="HTML5">
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-256/html5-41-1175209.png"
+            alt="HTML"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="CSS3 (Cascading Style Sheets)">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/730px-CSS.3.svg.png"
+            alt="CSS"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="JavaScript">
+          <img src={JavasScriptLogo} alt="JavaScript" />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="jQuery">
+          <img src="https://logodix.com/logo/941103.png" alt="jQuery" />
+        </Tooltip>
+        <br />
+        <Tooltip arrow placement="top" title="React.js">
+          <img
+            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+            alt="React"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Material UI">
+          <img src={MuiLogo} alt="Material UI" />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Styled-Components">
+          <img src={StyledComponentsLogo} alt="styled components" />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Bootstrap">
+          <img src={BootstrapLogo} alt="Bootstrap" />
+        </Tooltip>
+        <br />
+
+        <br />
+        <Tooltip arrow placement="top" title="Ruby">
+          <img src={RubyLogo} alt="ruby" />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Ruby on Rails">
+          <img src={RubyOnRailsLogo} alt="Rails" />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="PostgreSQL">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"
+            alt="postgresql"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Git">
+          <img
+            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+            alt="Git"
+          />
+        </Tooltip>
+        <br />
+        <Tooltip arrow placement="top" title="MongoDB">
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-512/mongodb-3-1175138.png"
+            alt="mongodb"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Node.js">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png"
+            alt="node"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Express">
+          <img
+            src="https://img2.pngio.com/express-js-png-5-png-image-expressjs-png-800_800.png"
+            alt="Express"
+          />
+        </Tooltip>
+        <Tooltip arrow placement="top" title="Axios">
+          <img
+            src="https://user-images.githubusercontent.com/8939680/57233882-20344080-6fe5-11e9-9086-d20a955bed59.png"
+            alt="Axios"
+          />
+        </Tooltip>
+        <br />
+      </div>
+    </Section>
+  );
+}
 
 const Section = styled.section`
   display: flex;
@@ -110,122 +221,3 @@ const Section = styled.section`
     margin-top: 100px;
   }
 `;
-
-function Skills() {
-  const [cloudMode] = useContext(CloudStateContext);
-
-  return (
-    <Section className="language" cloudMode={cloudMode}>
-      <div className="card">
-        <h1>
-          <Sparkles>
-            <div className="text-letter">S</div>
-            <div className="text-letter">k</div>
-            <div className="text-letter">i</div>
-            <div className="text-letter">l</div>
-            <div className="text-letter">l</div>
-            <div className="text-letter">s</div>
-          </Sparkles>
-        </h1>
-        <br />
-        <br />
-
-        <Tooltip arrow placement="top" title="HTML5">
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-256/html5-41-1175209.png"
-            alt="HTML"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="CSS3 (Cascading Style Sheets)">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/730px-CSS.3.svg.png"
-            alt="CSS"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="JavaScript">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
-            alt="JavaScript"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="jQuery">
-          <img src="https://logodix.com/logo/941103.png" alt="jQuery" />
-        </Tooltip>
-        <br />
-        <Tooltip arrow placement="top" title="React.js">
-          <img
-            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
-            alt="React"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Material UI">
-          <img src={MuiLogo} alt="Material UI" />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Styled-Components">
-          <img src={StyledComponentsLogo} alt="styled components" />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Bootstrap">
-          <img
-            src="https://camo.githubusercontent.com/a664defdd5c2ec93a3fbfb51e0f2aaafa5dc57bf1e13aa47456ced037b3cebe8/68747470733a2f2f676574626f6f7473747261702e636f6d2f646f63732f352e302f6173736574732f6272616e642f626f6f7473747261702d6c6f676f2d736861646f772e706e67"
-            alt="Bootstrap"
-          />
-        </Tooltip>
-        <br />
-
-        <br />
-        <Tooltip arrow placement="top" title="Ruby">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png"
-            alt="ruby"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Ruby on Rails">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png"
-            alt="Rails"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="PostgreSQL">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"
-            alt="postgresql"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Git">
-          <img
-            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
-            alt="Git"
-          />
-        </Tooltip>
-        <br />
-        <Tooltip arrow placement="top" title="MongoDB">
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-512/mongodb-3-1175138.png"
-            alt="mongodb"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Node.js">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png"
-            alt="node"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Express">
-          <img
-            src="https://img2.pngio.com/express-js-png-5-png-image-expressjs-png-800_800.png"
-            alt="Express"
-          />
-        </Tooltip>
-        <Tooltip arrow placement="top" title="Axios">
-          <img
-            src="https://user-images.githubusercontent.com/8939680/57233882-20344080-6fe5-11e9-9086-d20a955bed59.png"
-            alt="Axios"
-          />
-        </Tooltip>
-        <br />
-      </div>
-    </Section>
-  );
-}
-
-export default Skills;
