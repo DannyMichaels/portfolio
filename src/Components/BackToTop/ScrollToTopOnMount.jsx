9fe6de00b7c6aad8c6e5e9b3abe40fc8a146d92d
Daniel <itzdanielmichael@gmail.com>
2021-10-01 00:13:55 -0400

    update ScrollToTopOnMount.jsx

--- src/Components/BackToTop/ScrollToTopOnMount.jsx
+++ src/Components/BackToTop/ScrollToTopOnMount.jsx
@@ -1,7 +1,13 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
+
 export default function ScrollToTopOnMount() {
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.onbeforeunload = () => {
+      // this is for page refresh
+      window.scrollTo(0, 0);
+    };
+
+    window.scrollTo(0, 0); // for component change (no different pages yet in this app though...)
   }, []);
 
   return null;
